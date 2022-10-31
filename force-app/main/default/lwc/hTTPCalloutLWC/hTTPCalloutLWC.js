62e75fabd7f0dea2f381fd7173711b5084011986
seniorfulldev <piaoyilong114@gmail.com>
2022-10-31 23:49:43 +0800

    add LWC basic ui

--- force-app/main/default/lwc/hTTPCalloutLWC/hTTPCalloutLWC.js
+++ force-app/main/default/lwc/hTTPCalloutLWC/hTTPCalloutLWC.js
@@ -1,3 +1,10 @@
 import { LightningElement } from 'lwc';
-
+const options=
+[
+    {label: 'usd', value: USD},
+    {label: 'EUR', value: EUR},
+    {label: 'CAD', value: CAD},
+    {label: 'GBP', value: GBP},
+    {label: 'IMR', value: IMR},
+]
 export default class HTTPCalloutLWC extends LightningElement {}
